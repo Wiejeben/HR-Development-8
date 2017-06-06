b295de2a4ab89780f730594fedb8824913016a55
Maarten de Graaf <wiejeben@gmail.com>
2017-06-06 22:57:58 +0200

    Split project into Haskell and F# versions

--- Parser.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import ParseSVM
-
--- parses SVM file from standard input port and displays result
-main = do
-  input <- getContents -- reads from stdin
-  let result = parse program "(standard input)" input
-  print result
