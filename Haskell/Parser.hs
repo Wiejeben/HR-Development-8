4fe146eeb87549ea9143a30045ac71ebbf9bbb16
Maarten de Graaf <wiejeben@gmail.com>
2018-05-30 11:51:44 +0200

    Some progress

--- Haskell/Parser.hs
+++ Haskell/Parser.hs
@@ -1,9 +1,19 @@
--- module Main (main) where
+module Main (main) where
 
 import ParseSVM
+
+runCommands (x:xs) = runCommands (runCommand x) : runCommands xs
+
+executeProgram (Left n) = "eh"
+executeProgram (Right commands) = show (runCommands commands)
+
+-- executeProgram (Right (Mov (Register Reg1) (Location (Address (Direct 15)))) = "Test"
+-- 	| 
+-- 	| otherwise = show (length n)
 
 -- parses SVM file from standard input port and displays result
 main = do
     input <- getContents -- reads from stdin
     let result = parse program "(standard input)" input
-    print result
+    print (executeProgram result)
+
