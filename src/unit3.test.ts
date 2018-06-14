ec8224696e35471c4cd56bab630a6123333a92fe
Maarten de Graaf <wiejeben@gmail.com>
2018-06-14 17:24:00 +0200

    Implement merge sort

--- src/unit3.test.ts
+++ src/unit3.test.ts
@@ -1,9 +1,10 @@
 import { List, Cons, Empty } from './unit2';
-import { Tuple, splitAt, merge } from './unit3'
+import { Tuple, splitAt, merge, mergeSort } from './unit3'
 
 const foo = (): List<number> => Cons(3)(Cons(5)(Cons(4)(Cons(-1)(Cons(2)(Cons(2)(Empty()))))))
 const bar = (): List<number> => Cons(2)(Cons(4)(Cons(5)(Cons(6)(Empty()))))
 const baz = (): List<number> => Cons(1)(Cons(3)(Cons(7)(Cons(8)(Empty()))))
+const bax = (): List<number> => Cons(5)(Cons(2)(Cons(4)(Cons(3)(Cons(1)(Empty())))))
 
 describe('exercise 1', () => {
     it('should split list into two lists inside a tuple.', () => {
@@ -21,4 +22,12 @@ describe('exercise 2', () => {
 
         expect(merge(bar())(baz()).toString()).toBe(result.toString())
     })
-});
+})
+
+describe('exercise 3', () => {
+    it('should sort list based on the Merge Sort algorithm.', () => {
+        const result = Cons(1)(Cons(2)(Cons(3)(Cons(4)(Cons(5)(Empty())))))
+
+        expect(JSON.stringify(mergeSort(bax()))).toEqual(JSON.stringify(result))
+    })
+})
--- src/unit3.ts
+++ src/unit3.ts
@@ -15,8 +15,7 @@ export const Tuple = <a, b>(fst: a) => (snd: b): Tuple<a, b> => ({ fst: fst, snd
  */
 export const splitAt = <a>(i: number) => (l: List<a>): Tuple<List<a>, List<a>> =>
     (l.kind === 'cons') ?
-        (i <= 0) ?
-            Tuple<List<a>, List<a>>(Cons(l.head)(Empty()))(l.tail) :
+        (i <= 0) ? Tuple<List<a>, List<a>>(Cons(l.head)(Empty()))(l.tail) :
             ((t: Tuple<List<a>, List<a>>): Tuple<List<a>, List<a>> => Tuple<List<a>, List<a>>(Cons(l.head)(t.fst))(t.snd))(splitAt<a>(i - 1)(l.tail)) :
         undefined;
 
@@ -24,8 +23,27 @@ export const splitAt = <a>(i: number) => (l: List<a>): Tuple<List<a>, List<a>> =
  * Exercise 2:
  * Implement a function that merges together two sorted lists into a single sorted list.
  */
-export const merge = (l1: List<number>) => (l2: List<number>): List<number> =>
+export const merge = <a>(l1: List<a>) => (l2: List<a>): List<a> =>
     (l1.kind === 'empty') ? l2 :
         (l2.kind === 'empty') ? l1 :
             (l1.head <= l2.head) ? Cons(l1.head)(merge(l1.tail)(l2)) :
                 Cons(l2.head)(merge(l1)(l2.tail))
+
+/**
+ * Exercise 3:
+ * Implement a function that implements the Merge Sort algorithm. The Merge Sort returns the list itself
+ * when the list has only one element. Otherwise it splits l in two lists, one containing the lements
+ * between position 0 and l.length / 2. Merge Sort is called recursively  on the two lists. After this step
+ * the function merge above to merge the two sorted lists.
+ */
+export const mergeSort = <a>(l: List<a>): List<a> => {
+    const count = l.count();
+
+    if (count <= 1) {
+        return l;
+    }
+
+    const splitted = splitAt<a>((Math.floor(count / 2) - 1))(l);
+
+    return merge<a>(mergeSort<a>(splitted.fst))(mergeSort<a>(splitted.snd));
+}
